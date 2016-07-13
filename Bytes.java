a8777570058835714948d61ee22a39ce142a6a0f
Leonardo R Duarte <lramosduarte@gmail.com>
2016-07-12 23:09:39 -0300

    refatorado o metodo de conversao

--- Bytes.java
+++ Bytes.java
@@ -2,11 +2,11 @@ package utilString;
 
 public class Bytes{
     private static final char[] listaHex = "0123456789ABCDF".toCharArray();
-    public Static String byteToHexString(byte[] bytes) {
+    public static String byteToHexString(byte[] bytes) {
         char[] caracteresHex = new char[bytes.length * 2];
         int indicePar = 0;
-        for(char caractere : bytes){
-            int v = caractere & OxFF;
+        for(byte caractere : bytes){
+            int v = caractere & 0xFF;
             caracteresHex[indicePar * 2] = listaHex[v >>> 4];
             caracteresHex[indicePar * 2 + 1] = listaHex[v & 0X0F];
         }
